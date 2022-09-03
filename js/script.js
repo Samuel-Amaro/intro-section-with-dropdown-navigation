cc4628d28a594b509f1233efd85bfb56437c8caa
Samuel Amaro <samuelamaro96746313@gmail.com>
2022-09-02 21:50:04 -0300

    add side menu with css e js and aria-labels and roles to acessibility

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 "use strict";
 
 let btnOpenMenu = document.querySelector(".btn-menu");
+let linksNavigation = document.querySelectorAll(".navigation__link");
+
+//handler to dropdown
+linksNavigation.forEach((link) => {
+    if(link.dataset.toggle){
+        link.addEventListener("pointerdown", handlerLinksNavigation);
+    }
+});
 
 function handlerBtnMenu(event) {
     showMenuMobile();
@@ -34,4 +42,33 @@ function closeMenuMobile() {
     document.body.classList.remove("overlay");
 }
 
-//TODO: adicionar eventos de click para mostrar e ocultar dropfowm menu baseado no <a> elemento clicado so adicionar evento para aquele, que possui o atributo data-toggle com a classe do dropdown relacionado
+function handlerLinksNavigation(event) {
+    toggleStateDropdownMenu(event.target.dataset.toggle, event.target);
+}
+
+function toggleStateDropdownMenu(nameDropdown, link) {
+    let dropdown = document.querySelector(`.${nameDropdown}`);
+    if(dropdown.dataset.state === "hidden" && link.dataset.open === "false") {
+        dropdown.classList.remove("dropdown-close");
+        dropdown.classList.add("dropdown-open");
+        dropdown.dataset.state = "visibility";
+        link.dataset.open = "true";
+        toggleImgClickLink(link);
+    }
+    else if(dropdown.dataset.state === "visibility" && link.dataset.open === "true") {
+        dropdown.classList.remove("dropdown-open");
+        dropdown.classList.add("dropdown-close");
+        dropdown.dataset.state = "hidden";
+        link.dataset.open = "false";
+        toggleImgClickLink(link);
+    }
+}
+
+function toggleImgClickLink(element) {
+    let img = element.firstElementChild;
+    if(element.dataset.open === "false") { //arrow down
+        img.src = "./images/icon-arrow-down.svg";
+    }else if(element.dataset.open === "true") { //arrow up
+        img.src = "./images/icon-arrow-up.svg";
+    }
+}
